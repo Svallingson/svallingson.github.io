fdf700ac47831117e95c5e72671d25979101525c
svalan <erik.svallingson@protonmail.com>
2024-03-10 15:31:18 +0100

    add darkmode functionality

---
+++ js/darkmode.js
@@ -0,0 +1,29 @@
+let darkMode = localStorage.getItem("darkMode");
+
+const darkModeToggle = document.querySelector("#theme-toggle");
+
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+
+  localStorage.setItem("darkMode", "enabled");
+}
+
+const disableDarkMode = () => {
+  document.body.classList.remove("darkmode");
+
+  localStorage.setItem("darkMode", null);
+}
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
+darkModeToggle.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkMode");
+
+    if (darkMode !== "enabled") {
+      enableDarkMode();
+    } else {
+      disableDarkMode();
+    }
+  });
